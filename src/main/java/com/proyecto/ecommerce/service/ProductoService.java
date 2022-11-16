f6db91886a5597d8da03814de3d24cd5fda17e7d
Bilalmohamedali <bilalmohamedalipro@gmail.com>
2022-11-16 20:31:31 +0100

    Funcionalidad mostrar productos para el admin

--- src/main/java/com/proyecto/ecommerce/service/ProductoService.java
+++ src/main/java/com/proyecto/ecommerce/service/ProductoService.java
@@ -1,5 +1,7 @@
 package com.proyecto.ecommerce.service;
 
+import java.util.List;
+
 import java.util.Optional;
 
 import com.proyecto.ecommerce.model.Producto;
@@ -9,5 +11,6 @@ public interface ProductoService {
 	public Optional<Producto> get(Integer id);
 	public void update(Producto producto);
 	public void delete(Integer id);
+	public List<Producto> findAll();
 
 }
