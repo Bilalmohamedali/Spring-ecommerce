0b7c236868fbb4547092545e995f13af0c7c1fef
Bilalmohamedali <bilalmohamedalipro@gmail.com>
2022-11-17 21:14:23 +0100

    implementar las funcionalidades de cargar imagen y mostrar imagen para el admin

--- src/main/java/com/proyecto/ecommerce/controller/ProductoController.java
+++ src/main/java/com/proyecto/ecommerce/controller/ProductoController.java
@@ -56,14 +56,7 @@ public class ProductoController {
 			String nombreImagen= upload.saveImage(file);
 			producto.setImagen(nombreImagen);
 		}else {
-			if (file.isEmpty()) {	//editamos el producto pero no cambiamos la iamgen
-				Producto p= new Producto();
-				p=productoService.get(producto.getId()).get();
-				producto.setImagen(p.getImagen());
-			}else {
-				String nombreImagen= upload.saveImage(file);
-				producto.setImagen(nombreImagen);
-			}
+			
 		}
 		
 		
@@ -85,7 +78,24 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/update")
-	public String update(Producto producto) {
+	public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
+		Producto p= new Producto();
+		p=productoService.get(producto.getId()).get();
+		
+		if (file.isEmpty()) {	//editamos el producto pero no cambiamos la iamgen
+
+			producto.setImagen(p.getImagen());
+		}else {	//cuando se edita tambien la iamgen
+			
+			//Eliminar cuando la imagen no sea por defecto
+			if (!p.getImagen().equals("default.jpg")) {
+				upload.deleteImage(p.getImagen());
+				
+			}
+			String nombreImagen= upload.saveImage(file);
+			producto.setImagen(nombreImagen);
+		}
+		producto.setUsuario(p.getUsuario());
 		productoService.update(producto);
 		return "redirect:/productos";
 		
@@ -93,6 +103,16 @@ public class ProductoController {
 	
 	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable Integer id) {
+		
+		Producto p = new Producto();
+		p=productoService.get(id).get();
+		
+		//Eliminar cuando la imagen no sea por defecto
+		if (!p.getImagen().equals("default.jpg")) {
+			upload.deleteImage(p.getImagen());
+			
+		}
+		
 		productoService.delete(id);
 		return "redirect:/productos";
 	}
