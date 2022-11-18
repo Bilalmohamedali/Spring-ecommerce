75cf84b8cbc1723242784cccec5a55cfb2953a06
Bilalmohamedali <bilalmohamedalipro@gmail.com>
2022-11-18 20:27:31 +0100

    Añadimos redireccion vista orden

--- src/main/java/com/proyecto/ecommerce/controller/HomeController.java
+++ src/main/java/com/proyecto/ecommerce/controller/HomeController.java
@@ -129,5 +129,12 @@ public class HomeController {
 		model.addAttribute("orden", orden);
 		return "/usuario/carrito";
 	}
+	
+	@GetMapping("/order")
+	public String order() {
+		
+		return "usuario/resumenorden";
+	}
+	
 
 }
