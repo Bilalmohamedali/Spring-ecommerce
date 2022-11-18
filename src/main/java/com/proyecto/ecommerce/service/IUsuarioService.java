762c656c3d4d1078290b4fa743745e0994c65da2
Bilalmohamedali <bilalmohamedalipro@gmail.com>
2022-11-18 21:33:43 +0100

    Mostramos la informacion del usuario y el detalle del pedido en la vista resumen orden

---
+++ src/main/java/com/proyecto/ecommerce/service/IUsuarioService.java
@@ -0,0 +1,11 @@
+package com.proyecto.ecommerce.service;
+
+import java.util.Optional;
+
+import com.proyecto.ecommerce.model.Usuario;
+
+public interface IUsuarioService {
+	Optional<Usuario> findById(Integer id);
+	
+
+}
